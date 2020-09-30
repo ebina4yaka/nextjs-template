b4fa52c4145f8cc29e87b9fb2a74434192ee506f
ebina4yaka <4yaka3bina@gmail.com>
2020-09-30 13:55:36 +0900

    use typeface

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,12 +7,7 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head>
-          <link
-            rel="stylesheet"
-            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-          />
-        </Head>
+        <Head />
         <body>
           <Main />
           <NextScript />
