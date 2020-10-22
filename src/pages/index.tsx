e0c8ae14b8db84979029ec59474f471a7a40fed8
ebina4yaka <4yaka3bina@gmail.com>
2020-10-22 09:01:44 +0900

    fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,25 @@
 import React from 'react'
 import Typography from '@material-ui/core/Typography'
+import { Box, Container } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
 import Copyright from '../atoms/Coryright'
 
+const useStyles = makeStyles({
+  component: {
+    textAlign: 'center',
+  },
+})
+
 export default function Index(): React.ReactElement {
+  const classes = useStyles()
   return (
-    <>
-      <Typography variant="h4" component="h1" gutterBottom>
-        Next.js with TypeScript example
-      </Typography>
-      <Copyright />
-    </>
+    <Container maxWidth="lg">
+      <Box my={4} className={classes.component}>
+        <Typography variant="h4" component="h1" gutterBottom>
+          Next.js with TypeScript example
+        </Typography>
+        <Copyright />
+      </Box>
+    </Container>
   )
 }
