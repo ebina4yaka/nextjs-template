b6695d02ff19055a74ccafa201255b2d0c0109e7
ebina4yaka <4yaka3bina@gmail.com>
2021-03-03 13:56:23 +0900

    mock next/router in Storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,5 @@
 import '../src/styles/globals.css'
+import * as nextRouter from 'next/router'
 
 require('@storybook/react').addParameters({
   options: {
@@ -7,4 +8,14 @@ require('@storybook/react').addParameters({
         ? 0
         : a[1].id.localeCompare(b[1].id, undefined, { numeric: true }),
   },
+})
+
+nextRouter.useRouter = () => ({
+  route: '/',
+  pathname: '/about',
+  query: { query: 'Next.js Storybook' },
+  asPath: '',
+  basePath: '',
+  push: () => {},
+  prefetch: () => new Promise((resolve, reject) => {}),
 })
