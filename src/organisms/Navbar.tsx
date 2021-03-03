617502c079e7744467c08eb4ba277bca2c2b79ba
ebina4yaka <4yaka3bina@gmail.com>
2021-03-03 11:28:52 +0900

    Fixed style mobile menu

--- src/organisms/Navbar.tsx
+++ src/organisms/Navbar.tsx
@@ -3,56 +3,31 @@ import DarkModeToggle from '../molecules/DarkModeToggle'
 
 export default function Navbar(): ReactElement {
   return (
-    <nav className="bg-white dark:bg-gray-800">
-      <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
-        <div className="relative flex items-center justify-between h-16">
-          <div
-            className="flex-1 flex items-center
-           justify-center sm:items-stretch sm:justify-start"
-          >
+    <nav className="navbar">
+      <div className="navbar-size">
+        <div className="navbar-content">
+          <div className="navbar-left">
             <div className="flex-shrink-0 flex items-center">
-              <div className="text-2xl font-semibold">Template Project</div>
+              <div className="text-2xl font-semibold">Logo</div>
             </div>
             <div className="hidden sm:block sm:ml-6">
               <div className="flex space-x-4">
-                <a
-                  href="/"
-                  className="bg-gray-200 text-black px-3 py-2 rounded-md text-sm font-medium
-                  dark:bg-gray-900 dark:text-white"
-                >
+                <a href="/" className="navbar-selected">
                   Dashboard
                 </a>
-                <a
-                  href="/"
-                  className="text-white-300
-                   hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium
-                   dark:bg-gray-800 dark:hover:bg-white dark:hover:text-black"
-                >
+                <a href="/" className="navbar-no-selected">
                   Team
                 </a>
-                <a
-                  href="/"
-                  className="text-white-300
-                   hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium
-                   dark:bg-gray-800 dark:hover:bg-white dark:hover:text-black"
-                >
+                <a href="/" className="navbar-no-selected">
                   Projects
                 </a>
-                <a
-                  href="/"
-                  className="text-white-300
-                   hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium
-                   dark:bg-gray-800 dark:hover:bg-white dark:hover:text-black"
-                >
+                <a href="/" className="navbar-no-selected">
                   Calendar
                 </a>
               </div>
             </div>
           </div>
-          <div
-            className="absolute
-           inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0"
-          >
+          <div className="navbar-right">
             <DarkModeToggle />
           </div>
         </div>
@@ -60,31 +35,16 @@ export default function Navbar(): ReactElement {
 
       <div className="sm:hidden" id="mobile-menu">
         <div className="px-2 pt-2 pb-3 space-y-1">
-          <a
-            href="/"
-            className="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium"
-          >
+          <a href="/" className="navbar-selected block">
             Dashboard
           </a>
-          <a
-            href="/"
-            className="text-gray-300 hover:bg-gray-700
-             hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-          >
+          <a href="/" className="navbar-no-selected block">
             Team
           </a>
-          <a
-            href="/"
-            className="text-gray-300 hover:bg-gray-700
-             hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-          >
+          <a href="/" className="navbar-no-selected block">
             Projects
           </a>
-          <a
-            href="/"
-            className="text-gray-300 hover:bg-gray-700
-             hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-          >
+          <a href="/" className="navbar-no-selected block">
             Calendar
           </a>
         </div>
